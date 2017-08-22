6905808bface0b1cb2153f3b74c47fbbce19077f
0smo5is <28951652+0smo5is@users.noreply.github.com>
2017-08-22 11:50:28 -0500

    Update simplemud.py

--- simplemud.py
+++ simplemud.py
@@ -182,17 +182,18 @@ while True:
         # 'look' command
         elif command in ("look", "l", "ls"):
 
+            lw = params.lower()
             # stores items in room
-            it = params.lower()
+            it = rooms[players[id]["loot"]]
             # store the player's current room
             rm = rooms[players[id]["room"]]
 
             # send the player back the description of their current room
             mud.send_message(id, rm["description"])
 
-            if rm["loot"] != None:
+            if rm["loot"] is not None:
                 mud.send_message(id, "There is a {} here.".format(
-                                 rm["loot"][it]))
+                                 it["loot"][lw]))
 
             playershere = []
             # go through every player in the game
